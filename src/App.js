c1c42ac56d4ce24e824c1fbfd26fca2f97e189ae
jmurphy0 <murphbot55@gmail.com>
2021-03-26 16:02:30 -0400

    adding nav into index.html and bootstap js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Portfolio from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Router>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="Contact" component={Contact} />
+        <Route exact path="Portfolio" component={Portfolio} />
+      </Wrapper>
+      ;
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 
 export default function Wrapper(props) {
   return <main className="Wrapper">{props.children}</main>;
