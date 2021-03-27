20aafa8d3e9e741e0f49807d27ddcc5600a37bd2
jmurphy0 <murphbot55@gmail.com>
2021-03-27 13:20:59 -0400

    added contact page

---
+++ src/utils/Projectdata.js
@@ -0,0 +1,10 @@
+export const Projectdata = [
+  {
+    title: "Panther Chat",
+    description:
+      "A full-stack web application for chatting with friends or meeting new people!",
+    img: "imgs/main-room.JPG",
+    liveLink: "https://panther-chatroom.herokuapp.com/",
+    githubLink: "https://github.com/PurplePanthers/PantherChatroom.git",
+  },
+];
