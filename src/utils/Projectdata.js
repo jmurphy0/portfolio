3bd85d20e5b2cfac2046c70f004321ddb0893ea9
jmurphy0 <murphbot55@gmail.com>
2021-03-27 14:28:53 -0400

    adding more project to Projectdata with minor styling changes

--- src/utils/Projectdata.js
+++ src/utils/Projectdata.js
@@ -15,5 +15,43 @@ const Projectdata = [
     liveLink: "https://note-taker-murphy.herokuapp.com/",
     githubLink: "https://github.com/jmurphy0/hmwk10-NoteTaker.git",
   },
+  {
+    title: "Art Mapper",
+    description:
+      "An app that allows the user to search the Harvard Art Museum paintings and save favorites to local storage. Users can also explore nearby galleries.",
+    img: "imgs/p1pic1.png",
+    liveLink: "https://jmurphy0.github.io/project-1/",
+    githubLink: "https://jmurphy0.github.io/project-1/",
+  },
+  {
+    title: "Weather Dashoard",
+    description:
+      "Weather dashboard displays the current and forcasted weather for the city determined by the userinput. Previus searches will be stored in local storage and apear as buttons for convient access to user.",
+    img: "imgs/weatherDash.png",
+    liveLink: "https://jmurphy0.github.io/Homework-6/",
+    githubLink: "https://jmurphy0.github.io/Homework-6/",
+  },
+  {
+    title: "Workday Planner",
+    description:
+      "A simple workday scheduling app to keep you productive and on time!",
+    img: "imgs/plannerPic.png",
+    liveLink: "https://jmurphy0.github.io/homework5/",
+    githubLink: "https://jmurphy0.github.io/homework5/",
+  },
+  // {
+  //   title: "",
+  //   description: "",
+  //   img: "",
+  //   liveLink: "",
+  //   githubLink: "",
+  // },
+  // {
+  //   title: "",
+  //   description: "",
+  //   img: "",
+  //   liveLink: "",
+  //   githubLink: "",
+  // },
 ];
 export default Projectdata;
