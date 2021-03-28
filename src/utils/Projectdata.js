23b8ae588f1da555fcffdd6bade34949a143e9e5
jmurphy0 <murphbot55@gmail.com>
2021-03-28 15:30:50 -0400

    fixed image issues

--- src/utils/Projectdata.js
+++ src/utils/Projectdata.js
@@ -1,4 +1,12 @@
 import PantherImg from "../imgs/main-room.JPG";
+import EmpImg from "../imgs/directory.png";
+import Budget from "../imgs/budget.png";
+import Workout from "../imgs/stats.png";
+import Note from "../imgs/notTaker.png";
+import Art from "../imgs/p1pic1.png";
+import Weather from "../imgs/weatherDash.png";
+import Planner from "../imgs/plannerPic.png";
+import { propTypes } from "react-bootstrap/esm/Image";
 const Projectdata = [
   {
     title: "Panther Chat",
@@ -12,7 +20,7 @@ const Projectdata = [
     title: "Employee Directory",
     description:
       "A react project using React Bootstrap Datatables exploring sorting and searching from API data.",
-    img: "img/directory.png",
+    img: EmpImg,
     liveLink: "https://jmurphy0.github.io/employeedirectory/",
     githubLink: "https://github.com/jmurphy0/gooddirectory.git",
   },
@@ -20,7 +28,7 @@ const Projectdata = [
     title: "Budget Tracker",
     description:
       "Keeps track of your money, regadless of your internet connection using index db. Deployed on Heroku.",
-    img: "./budget.png",
+    img: Budget,
     liveLink: "https://murphybudget.herokuapp.com/",
     githubLink: "https://github.com/jmurphy0/budgetTracker.git",
   },
@@ -28,7 +36,7 @@ const Projectdata = [
     title: "Fitness Dahboard",
     description:
       "This app is designed to keep track of your workouts (stored to mongoatlas) and allows your to view results of total duration and weight lifted. Deployed on heroku.",
-    img: "imgs/stats.png",
+    img: Workout,
     liveLink:
       "https://murphyworkout.herokuapp.com/exercise?id=6052776c2b9b1a00159ae570",
     githubLink: "https://github.com/jmurphy0/workoutTracker.git",
@@ -38,14 +46,14 @@ const Projectdata = [
     description:
       "A simple app to takes notes online. Deployed by heroku and ready to keep you organized!",
     img: "imgs/notTaker.png",
-    liveLink: "https://note-taker-murphy.herokuapp.com/",
+    liveLink: Note,
     githubLink: "https://github.com/jmurphy0/hmwk10-NoteTaker.git",
   },
   {
     title: "Art Mapper",
     description:
       "An app that allows the user to search the Harvard Art Museum paintings and save favorites to local storage. Users can also explore nearby galleries.",
-    img: "imgs/p1pic1.png",
+    img: Art,
     liveLink: "https://jmurphy0.github.io/project-1/",
     githubLink: "https://jmurphy0.github.io/project-1/",
   },
@@ -53,7 +61,7 @@ const Projectdata = [
     title: "Weather Dashoard",
     description:
       "Weather dashboard displays the current and forcasted weather for the city determined by the userinput. Previus searches will be stored in local storage and apear as buttons for convient access to user.",
-    img: "imgs/weatherDash.png",
+    img: Weather,
     liveLink: "https://jmurphy0.github.io/Homework-6/",
     githubLink: "https://jmurphy0.github.io/Homework-6/",
   },
@@ -61,7 +69,7 @@ const Projectdata = [
     title: "Workday Planner",
     description:
       "A simple workday scheduling app to keep you productive and on time!",
-    img: "imgs/plannerPic.png",
+    img: Planner,
     liveLink: "https://jmurphy0.github.io/homework5/",
     githubLink: "https://jmurphy0.github.io/homework5/",
   },
