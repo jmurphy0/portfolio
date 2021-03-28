b940ef6129a6889b95cca6bbe30b40e18f300e23
jmurphy0 <murphbot55@gmail.com>
2021-03-28 15:08:17 -0400

    trying

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../../components/Hero";
 import "./style.css";
 import Path from "path";
-import myPic from "../../../public/myPic.jpeg";
+import myPic from "../../imgs/myPic.jpeg";
 // returns a card contation the about me information. wrapped in the hero background image.
 function About() {
   return (
