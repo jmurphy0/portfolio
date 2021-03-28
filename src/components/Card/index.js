e984e288cac8b5ca84369f5faeab7101c36a276d
jmurphy0 <murphbot55@gmail.com>
2021-03-28 13:15:44 -0400

    commenting cards.js component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
-
+// card component takes project info and populates a card with
+// the title, img, description, github link, and livelink.
 function Card(props) {
-  console.log(props);
   return (
     <section class="col-md-6 mb-5" id="card">
       <a href={props.liveLink} target="_blank">
-        <img src={props.img} class="img-thumbnail" alt="PantherChatroom" />
+        <img src={props.img} class="img-thumbnail" alt={props.name} />
       </a>
       <a href={props.githubLink} target="_blank">
         <p class="ptitle">{props.title}</p>
