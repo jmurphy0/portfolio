6e208d958c5081dd96f5181a68f95b7a29f1ca62
jmurphy0 <murphbot55@gmail.com>
2021-03-28 13:18:48 -0400

    commenting the index.js for About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../../components/Hero";
 import "./style.css";
-
+// returns a card contation the about me information. wrapped in the hero background image.
 function About() {
   return (
     <Hero>
