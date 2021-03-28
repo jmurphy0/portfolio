062188f75a13f98b901d8497ab9786ff3f6a0d1b
jmurphy0 <murphbot55@gmail.com>
2021-03-28 13:17:36 -0400

    commenting Wrapper component

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -2,7 +2,7 @@ import React from "react";
 //import { propTypes } from "react-bootstrap/esm/Image";
 import "./style.css";
 // import Hero from "../Hero";
-
+// a simple wrapper component wrapps all but the navbars in a div
 function Wrapper(props) {
   return <div className="Wrapper">{props.children}</div>;
 }
